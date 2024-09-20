51db23316cec6616574c972a14f03448f388df79
itimbers <itimbers@uwyo.edu>
2024-09-20 00:59:04 +0000

    submitting...

---
+++ Lab1.py
@@ -0,0 +1,13 @@
+firstname = "Isaiah"
+lastname = "Timbers"
+
+age = 21 #integer
+
+num = 1.234 #float
+
+isIsaiahAGoodHEO = False
+
+print("My name is",firstname, lastname)
+
+agePlusNum = age + num
+print(agePlusNum)
